34406c8839ca3b50ab405abd3289466636addf23
dhaanisi <dhanishende2304@gmail.com>
2026-03-07 22:32:46 +0530

    instrumentation-client update for deployment

--- packages/analytics/instrumentation-client.ts
+++ packages/analytics/instrumentation-client.ts
@@ -2,8 +2,14 @@ import posthog from "posthog-js";
 import { keys } from "./keys";
 
 export const initializeAnalytics = () => {
-  posthog.init(keys().NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+  const env = keys();
+
+  if (!env.NEXT_PUBLIC_POSTHOG_KEY || !env.NEXT_PUBLIC_POSTHOG_HOST) {
+    return;
+  }
+
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: "2025-05-24",
   });
 };
